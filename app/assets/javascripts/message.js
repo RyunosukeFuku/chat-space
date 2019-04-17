d2650d3dc409054e83a310a317071755689d2a0e
RyunosukeFuku <rfutian64@gmail.com>
2019-04-17 13:46:19 +0900

    インデント調整

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,11 @@
 
-$(function(){
+$(function ()
+{
 
-  function buildHTML(message){
-//functionによって関数を宣言,buildHTMLは関数の名前,(message)は引数
-    
+  function buildHTML(message)
+  {
+    //functionによって関数を宣言,buildHTMLは関数の名前,(message)は引数
+
     var image = message.image == null ? "" : `<img src="${message.image}" class="lower-message__image">`
 
     var html = `<div class = "message" data-id=${message.id}>
@@ -24,23 +26,24 @@ $(function(){
                 </div>`
     return html;
   }
-//htmlという変数を作った
-//実際のmessage.html.hamlと同じクラスや構成にしないとCSSが適用されない
+  //htmlという変数を作った
+  //実際のmessage.html.hamlと同じクラスや構成にしないとCSSが適用されない
 
 
 
-//非同期通信
-  $('#new_message').on('submit', function(e){
-//submitイベントで発火させる    
+  //非同期通信
+  $('#new_message').on('submit', function (e)
+  {
+    //submitイベントで発火させる    
     e.preventDefault(); //デフォルトで送信されるフォームの送信をキャンセル
     var formData = new FormData(this);
-//new fromDataでインスタンス作成    
-//引数thisはイベント発生したDOM要素#new_messageのフォーム情報取得   
+    //new fromDataでインスタンス作成    
+    //引数thisはイベント発生したDOM要素#new_messageのフォーム情報取得   
     var url = $(this).attr('action')
-//ajaxで使う変数urlにaction属性のurlを取得し代入    
+    //ajaxで使う変数urlにaction属性のurlを取得し代入    
     $.ajax({
-//フォーム送信押してajax通信
-//サーバーからデータ取得
+      //フォーム送信押してajax通信
+      //サーバーからデータ取得
       url: url, //リクエストのパス
       type: "POST", //HTTPメソッド
       data: formData, //リクエストと一緒に送るデータ
@@ -48,48 +51,54 @@ $(function(){
       processData: false,
       contentType: false, //リクエストに含まれるデータの型を変更しないための記述
     })
-   
-    .done(function(data){
-//返ってきたjsonをdoneメソッドで受け取る      
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $('.form__submit').prop( 'disabled', false );
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-      $('.new_message')[0].reset();
-     
-    })
 
-    .fail(function(){
-      alert('error');
-           
-    })
+      .done(function (data)
+      {
+        //返ってきたjsonをdoneメソッドで受け取る      
+        var html = buildHTML(data);
+        $('.messages').append(html);
+        $('.form__submit').prop('disabled', false);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight }, 'fast');
+        $('.new_message')[0].reset();
+
+      })
+
+      .fail(function ()
+      {
+        alert('error');
+
+      })
   });
 
-  function reloadMessages() {
-    
+  function reloadMessages()
+  {
+
     var lastMessageId = $(".message:last-child").data("id")
     console.log(lastMessageId);
     $.ajax({
       url: 'api/messages',
       type: 'get',
       dataType: 'json',
-      data: {id: lastMessageId},
+      data: { id: lastMessageId },
     })
-    .done(function(messages){
-      
-      var insertHTML = '';
-      messages.forEach(function(message){
-        var html = buildHTML(message);
-        insertHTML += html
+      .done(function (messages)
+      {
+
+        var insertHTML = '';
+        messages.forEach(function (message)
+        {
+          var html = buildHTML(message);
+          insertHTML += html
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight }, 'fast');
+
+      })
+      .fail(function ()
+      {
+        alert('error');
+
       });
-       $('.messages').append(insertHTML);
-       $('.messages').animate({scrollTop: $('.messages')[0]. scrollHeight}, 'fast');
-       
-    })
-    .fail(function() {
-      alert('error');
-       
-    });
   }
   setInterval(reloadMessages, 5000);
 });
